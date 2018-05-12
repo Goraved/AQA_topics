8eb839eea625df2a51a8345bf824b33352abd7e4
Roman Pobotin <rpobotin@adyax.com>
2018-05-12 22:18:45 +0300

    sql lng

--- data.py
+++ data.py
@@ -9,6 +9,13 @@ def query(sql):
                          database='aqatopics', connect_timeout=600)
     try:
         cursor = db.cursor()
+        cursor.execute("""SET NAMES 'utf8';
+    SET CHARACTER SET 'utf8';
+    SET SESSION collation_connection = 'utf8_general_ci';""")
+    except:
+        pass
+    try:
+        cursor = db.cursor()
         cursor.execute(sql)
     except (AttributeError, MySQLdb.OperationalError):
         db.ping(True)

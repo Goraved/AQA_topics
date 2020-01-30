0304cf06f93115b00701f166c5b1588d7f3fce68
Goraved <roman.pobotin@gmail.com>
2020-01-30 13:56:11 +0200

    Add error handling and pages

--- app.py
+++ app.py
@@ -105,5 +105,15 @@ def add_category():
     return redirect("/god")
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html', title='404'), 404
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', title='500'), 500
+
+
 if __name__ == "__main__":
     app.run()

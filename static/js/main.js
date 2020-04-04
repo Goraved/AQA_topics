3d9539dc15a3d04179f4a64c9a9efae551329318
Goraved <roman.pobotin@gmail.com>
2020-04-04 13:45:50 +0300

    Add categories scroll

--- static/js/main.js
+++ static/js/main.js
@@ -52,7 +52,7 @@ $('a[href*="#"]')
 
   // Get the modal
 var modal = document.getElementById('myModal');
-
+var categories = document.getElementById('id01');
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
@@ -71,8 +71,9 @@ span.onclick = function() {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == modal || event.target == categories) {
         modal.style.display = "none";
+        categories.style.display = "none";
     }
 }
 
@@ -113,3 +114,34 @@ var currentLink = $(this)[0].href;
 // end
   });
 
+var controller = new ScrollMagic.Controller();
+
+	// build tween
+	var tween = TweenMax.from("#animate", 0.5, {autoAlpha: 0, scale: 0.7});
+
+	// build scene
+	var scene = new ScrollMagic.Scene({triggerElement: "a#top", duration: 200, triggerHook: "onLeave"})
+					.setTween(tween)
+					.addIndicators() // add indicators (requires plugin)
+					.addTo(controller);
+
+	// change behaviour of controller to animate scroll instead of jump
+	controller.scrollTo(function (newpos) {
+		TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
+	});
+
+	//  bind scroll to anchor links
+	$(document).on("click", "a[href^='#']", function (e) {
+		var id = $(this).attr("href");
+		if ($(id).length > 0) {
+			e.preventDefault();
+
+			// trigger scroll
+			controller.scrollTo(id);
+
+				// if supported by the browser we can even update the URL.
+			if (window.history && window.history.pushState) {
+				history.pushState("", document.title, id);
+			}
+		}
+	});
